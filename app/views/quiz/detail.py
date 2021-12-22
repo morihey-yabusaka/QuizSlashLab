98431e8e7845d89d318f8911bdca27c58ddcfc6e
morihey-yabusaka <moritakohey@gmail.com>
2021-12-22 16:17:09 +0900

    update slash model

--- app/views/quiz/detail.py
+++ app/views/quiz/detail.py
@@ -1,7 +1,7 @@
 from django.views.generic import DetailView
 
 
-from ...models import Quiz
+from ...models import Quiz, Slash
 
 class QuizDetailView(DetailView):
   model = Quiz
@@ -14,6 +14,21 @@ class QuizDetailView(DetailView):
     for i in range(len(question)):
       n = Quiz.objects.filter(question__istartswith=question[:i+1]).count()-1
       context['n_start_equals'].append(n)
+
+    slashes = Slash.objects.filter(quiz=context['object'])
+    slashes = list(slashes.values())
+    context['slashes'] = []
+    for slash in slashes:
+      tmp = {}
+      for k,v in slash.items():
+        tmp[k] = v
+      if slash['n_push'] != 0:
+        correct_ratio = slash['n_correct'] / slash['n_push'] * 100
+        correct_ratio = str(correct_ratio).split(".")[0] + "%"
+      else:
+        correct_ratio = "-"
+      tmp['correct_ratio'] = correct_ratio
+      context['slashes'].append(tmp)
     return context
 
 quiz_detail_view = QuizDetailView.as_view()
