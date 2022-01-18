13e3011e264297072f75ad4ff0fb5cc7bc985a1f
morihey-yabusaka <moritakohey@gmail.com>
2022-01-18 17:33:50 +0900

    create deleteview and copy button

--- app/views/quiz/delete.py
+++ app/views/quiz/delete.py
@@ -1,10 +1,15 @@
+from django.urls import reverse_lazy
 from django.views.generic import DeleteView
 
 from ...models import Quiz
+from ...mixins import AuthorOnlyEditableMixin
 
 
-class QuizDeleteView(DeleteView):
+class QuizDeleteView(AuthorOnlyEditableMixin, DeleteView):
   model = Quiz
   template_name = "quiz/delete.html"
+
+  def get_success_url(self):
+    return reverse_lazy('app:quiz_list')
 
 quiz_delete_view = QuizDeleteView.as_view()
