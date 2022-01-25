af6b1e704e1d6eca655ce53c17d912b2a6e93492
morihey-yabusaka <moritakohey@gmail.com>
2022-01-25 20:02:57 +0900

    update quiz updateview

--- app/views/quiz/update.py
+++ app/views/quiz/update.py
@@ -2,14 +2,15 @@ from django.urls import reverse
 from django.views.generic import UpdateView
 
 from ...models import Quiz
+from ...forms import QuizForm
 from ...mixins import AuthorOnlyEditableMixin
 
 class QuizUpdateView(AuthorOnlyEditableMixin, UpdateView):
   model = Quiz
+  form_class = QuizForm
   template_name = 'quiz/update.html'
-  fields = ['question', 'answer', 'answer_yomi']
 
   def get_success_url(self):
-      return reverse('quiz_detail', kwargs={'pk': self.object.pk})
+      return reverse('app:quiz_detail', kwargs={'pk': self.object.pk})
 
 quiz_update_view = QuizUpdateView.as_view()
