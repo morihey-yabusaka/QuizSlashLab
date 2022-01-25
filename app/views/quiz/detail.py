a63da9a73f7442713f46bc6e2596b6de8e3abd20
morihey-yabusaka <moritakohey@gmail.com>
2022-01-25 17:52:24 +0900

    update quiz publish draft button

--- app/views/quiz/detail.py
+++ app/views/quiz/detail.py
@@ -1,4 +1,6 @@
+from django.urls import reverse
 from django.views.generic import DetailView
+from django.http import HttpResponseRedirect
 
 
 from ...models import Quiz, Slash
@@ -30,5 +32,17 @@ class QuizDetailView(DetailView):
       tmp['correct_ratio'] = correct_ratio
       context['slashes'].append(tmp)
     return context
+
+  def post(self, request, *args, **kwargs):
+    obj = self.get_object()
+    if 'draft' in request.POST:
+      obj.is_draft = True
+    elif 'publish' in request.POST:
+      obj.is_draft = False
+
+    obj.save()
+
+    return HttpResponseRedirect(reverse('app:quiz_detail', kwargs={'pk': obj.pk}))
+
 
 quiz_detail_view = QuizDetailView.as_view()
