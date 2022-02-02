cdc4651bc8dc37e9ad746b34ae139bb4d66fbf1c
morihey-yabusaka <moritakohey@gmail.com>
2022-02-02 13:44:18 +0900

    fix quiz search and quiz list design

--- app/static/js/form-clear.js
+++ app/static/js/form-clear.js
@@ -1,8 +1,9 @@
+const clearMembers = ["INPUT", "TEXTAREA"]
+
 window.addEventListener("DOMContentLoaded", _ => {
   var clearBtns = getElms('form button[clear]')
   for(var clearBtn of clearBtns) {
     clearBtn.addEventListener("click", e => {
-      clearMembers = ["INPUT", "TEXTAREA"]
       for(var formMember of e.currentTarget.form) {
         if (clearMembers.includes(formMember.tagName)) {
           formMember.value = ""
--- app/views/quiz/list.py
+++ app/views/quiz/list.py
@@ -6,19 +6,90 @@ from ...models import Quiz
 class QuizListView(ListView):
     model = Quiz
     context_object_name = 'quizes'
-    ordering = ['-created_at']
+    ordering = ['-updated_at']
     template_name = "quiz/list.html"
 
     def get_queryset(self):
         queryset = super().get_queryset()
-        question_query = self.request.GET.get('question')
-        answer_query = self.request.GET.get('answer')
+        # queryset.filter(is_draft=False, is_publish=True
+        get = self.request.GET # type(self.request.GET) == dict
+        question_query = get.get('question')
+        question_oa_query = get.get('q')
+        is_start_query = get.get('is_start')
+        answer_query = get.get('answer')
+        answer_oa_query = get.get('a')
+        question_answer_oa_query = get.get('qa')
 
-        # question and answer
-        queryset = queryset.filter(Q(question__icontains=question_query)) if question_query else queryset
-        queryset = queryset.filter(Q(answer__icontains=answer_query)) if answer_query else queryset
+        qq = Q()
+        if question_query:
+            question_query.strip()
+            if is_start_query:
+                qq &= Q(question__istartswith=question_query)
+            else:
+                question_query_list = question_query.replace('　', ' ').split(' ')
+                if 'or' == question_oa_query:
+                    for i, qq_ in enumerate(question_query_list):
+                        if i == 0:
+                            qq &= Q(question__icontains=qq_)
+                        else:
+                            qq |= Q(question__icontains=qq_)
+                elif 'and' == question_oa_query:
+                    for qq_ in question_query_list:
+                        qq &= Q(question__icontains=qq_)
+
+        aq = Q()
+        if answer_query:
+            answer_query.strip()
+            answer_query_list = answer_query.replace('　', ' ').split(' ')
+            if 'or' == answer_oa_query:
+                for i, aq_ in enumerate(answer_query_list):
+                    if i == 0:
+                        aq &= Q(answer__icontains=aq_)
+                    else:
+                        aq |= Q(answer__icontains=aq_)
+            elif 'and' == answer_oa_query:
+                for aq_ in answer_query_list:
+                    aq &= Q(answer__icontains=aq_)
+
+        q = Q()
+        if 'or' == question_answer_oa_query:
+            q &= qq | aq
+        elif 'and' == question_answer_oa_query:
+            q &= qq & aq
+
+        queryset = queryset.filter(q)
 
         return queryset
 
+    def get_context_data(self):
+        context = super().get_context_data()
+        get = self.request.GET
+        question_oa_query = get.get('q')
+        is_start_query = get.get('is_start')
+        answer_oa_query = get.get('a')
+        question_answer_oa_query = get.get('qa')
+
+        if 'or' == question_oa_query:
+            context['q_or'] = 'checked'
+            context['q_and'] = ''
+        else:
+            context['q_or'] = ''
+            context['q_and'] = 'checked'
+        if 'or' == answer_oa_query:
+            context['a_or'] = 'checked'
+            context['a_and'] = ''
+        else:
+            context['a_or'] = ''
+            context['a_and'] = 'checked'
+        if 'or' == question_answer_oa_query:
+            context['qa_or'] = 'checked'
+            context['qa_and'] = ''
+        else:
+            context['qa_or'] = ''
+            context['qa_and'] = 'checked'
+
+        context['is_start'] = 'checked' if is_start_query == 'on' else ''
+
+        return context
 
 quiz_list_view = QuizListView.as_view()
