3deeb1406b3e92513fab1ce2bf0c77f4ae127118
morihey-yabusaka <moritakohey@gmail.com>
2022-02-09 13:36:07 +0900

    update userpage

--- app/views/users/userpage.py
+++ app/views/users/userpage.py
@@ -1,6 +1,7 @@
 from django.views.generic import DetailView
 
 from users.models import User
+from ...models import Quiz
 
 
 class UserPageView(DetailView):
@@ -8,5 +9,14 @@ class UserPageView(DetailView):
   template_name = 'users/userpage.html'
   slug_field = 'username'
   context_object_name = "user_"
+
+  def get_context_data(self, **kwargs):
+    context = super().get_context_data(**kwargs)
+    user = self.get_object()
+    updated = Quiz.objects.filter(author=user, is_draft=False).order_by('updated_at')[:5]
+    context['updated'] = updated
+
+    return context
+
 
 user_page_view = UserPageView.as_view()
