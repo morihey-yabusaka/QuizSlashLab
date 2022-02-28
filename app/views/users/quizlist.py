d56a6f88f9ba82cb88a928be154af24ae1a7e0cc
morihey-yabusaka <moritakohey@gmail.com>
2022-02-28 17:41:05 +0900

    update userpage

--- app/views/users/quizlist.py
+++ app/views/users/quizlist.py
@@ -1,24 +1,22 @@
 from django.views.generic import ListView
 
 from ...models import Quiz
-
+from users.models import User
 
 class UserQuizListView(ListView):
   model = Quiz
   context_object_name = 'quizes'
-  ordering = ['-created_at']
   template_name = "quiz/list.html"
 
   def get_queryset(self, **kwargs):
-    queryset = super().get_queryset(**kwargs)
-    queryset = queryset.filter(is_draft=False, author=self.request.user)
+    queryset = User.objects.get(username=self.kwargs.get('slug')).quiz.publish().order_by('-updated_at')
     return queryset
 
   def get_context_data(self, **kwargs):
     context = super().get_context_data(**kwargs)
 
     context['title'] = [
-      {'text': self.request.user.username + 'の', 's': False},
+      {'text': self.kwargs.get('slug') + 'の', 's': False},
       {'text': '作', 's': True},
       {'text': '問', 's': False},
     ]
