a58ad0000449384f30435883adeff51bb8dadd4a
morihey-yabusaka <moritakohey@gmail.com>
2022-03-02 15:11:49 +0900

    update create view & popup

--- app/static/js/popup.js
+++ app/static/js/popup.js
@@ -1,11 +1,9 @@
 window.addEventListener("DOMContentLoaded", _ => {
   var popupElms = getElms(".popup")
   for(var popupElm of popupElms) {
-    console.log(popupElm)
     var popupTriger = popupElm.querySelector(".click")
-    console.log(popupTriger)
     popupTriger.addEventListener("click", e => {
-      popupElm.classList.toggle("on")
+      e.currentTarget.parentElement.classList.toggle("on")
     })
   }
 })
