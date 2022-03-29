0e361f449b33e3b8d0bedff582cc9ce3d7191c0f
morihey-yabusaka <moritakohey@gmail.com>
2022-03-29 10:56:07 +0900

    email settings debug

--- QuizSlashLab/settings.py
+++ QuizSlashLab/settings.py
@@ -165,7 +165,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.googlemail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'QuizSlashLab@gmail.com'
+EMAIL_HOST_USER = 'QuizSlashLab'
 
 # if local, debug == True
 try:
